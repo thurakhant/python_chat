7a4c572a1377f8dbbb1e185f6ecc0e8157744d68
thurakhant <thurakhantthein4425@gmail.com>
2021-08-27 02:20:48 +0630

    chat app beta v1

---
+++ client.py
@@ -0,0 +1,32 @@
+import socket
+import threading
+nickname = input("Choose your nickname: ")
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(('127.0.0.1', 7976))
+
+
+def receive():
+    while True:
+        try:
+            message = client.recv(1024).decode('ascii')
+            if message == 'NICKNAME':
+                client.send(nickname.encode('ascii'))
+            else:
+                print(message)
+        except:
+            print("An error occured!")
+            client.close()
+            break
+
+
+def write():
+    while True:
+        message = '{}: {} \n----------'.format(nickname, input(''))
+        client.send(message.encode('ascii'))
+
+
+receive_thread = threading.Thread(target=receive)
+receive_thread.start()
+write_thread = threading.Thread(target=write)
+write_thread.start()
